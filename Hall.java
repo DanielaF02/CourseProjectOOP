b5ecaa32fa85b2cbaf769df357b6115082af788e
Daniela Fereshtanova <didka416@gmail.com>
2023-03-05 19:53:06 +0200

    Created Hall Class

---
+++ Hall.java
@@ -0,0 +1,35 @@
+public class Hall {
+    private int id;
+    private int rowsCount;
+    private int seatsPerRow;
+
+    public int getId() {
+        return id;
+    }
+
+    public int rowsCount() {
+        return rowsCount;
+    }
+
+    public int seatsPerRow() {
+        return seatsPerRow;
+    }
+    
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    public void setRowsCount(int newRowsCount) {
+        this.id = newRowsCount;
+    }
+
+    public void setSeatsPerRow(int newSeatsPerRow) {
+        this.id = newSeatsPerRow;
+    }
+
+    public Hall(int newId, int newRowsCount, int newSeatsPerRow) {
+        this.id = newId;
+        this.rowsCount = newRowsCount;
+        this.seatsPerRow = newSeatsPerRow;
+    }
+}
